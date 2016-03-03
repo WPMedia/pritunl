9a43e2c59bc9fc326bc790107dbcf13b61a39c23
Patrick Cullen <patrick.cullen@washpost.com>
2016-03-03 17:52:12 -0500

    added PRITUNL_HOSTNAME environment variable to simplify upgrade process

--- pritunl/host/utils.py
+++ pritunl/host/utils.py
@@ -93,7 +93,7 @@ def iter_hosts_dict(page=None):
         yield hst.dict()
 
 def init():
-    settings.local.host = Host()
+    settings.local.host = Host(name=os.environ.get('PRITUNL_HOSTNAME', None))
 
     try:
         settings.local.host.load()
